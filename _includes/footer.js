67710751bf753f3772f8751033f5f5a33c88e091
Mike Hsu <mike.hsu@gmail.com>
2021-05-08 22:28:32 -0700

    add github link

--- _includes/footer.js
+++ _includes/footer.js
@@ -1,13 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>Mike Hsu's blog</p>
       <ul>
         <li>
-          <Image src="/images/Github-Mark-32px.png" width={24} height={24} />
-          Github
+          <Link href="https://github.com/hymm">
+            <a className={styles["github-link"]}>
+              <Image
+                src="/images/Github-Mark-32px.png"
+                width={24}
+                height={24}
+              />
+              <span>Github</span>
+            </a>
+          </Link>
         </li>
       </ul>
     </footer>
